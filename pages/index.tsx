a731e25d08531a43a4ad59dfcc062f7fa0b51e13
Ivoine <ivoinestrachan242@gmail.com>
2024-07-15 16:40:49 -0400

    updating site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 const Index = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow">Enter</div>
-      <div className="flex justify-center items-center py-4">
-        <button className="bg-gray-200 text-black px-8 py-3 rounded-full text-lg font-semibold shadow-md hover:bg-black hover:text-white">
+      <div className="flex-grow justify-center text-center mt-5">The Fit for you</div>
+      <div className="flex justify-center items-center py-20">
+        <button className="bg-gray-200 text-black px-8 py-3 rounded-full text-lg font-medium shadow-md hover:bg-black hover:text-white">
           Pre-Order
         </button>
       </div>
