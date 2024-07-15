ecc52b94f55b8809a0fb80f0b31907cc808f159f
Ivoine <ivoinestrachan242@gmail.com>
2024-07-15 14:31:05 -0400

    getting it out therE

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+const Index = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">Enter</div>
+      <div className="flex justify-center items-center py-4">
+        <button className="bg-gray-200 text-black px-8 py-3 rounded-full text-lg font-semibold shadow-md hover:bg-black hover:text-white">
+          Pre-Order
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
