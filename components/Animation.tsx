c501499955c94de8a2abebebfa254969b0279d88
Ivoine <ivoinestrachan242@gmail.com>
2024-09-18 08:47:28 -0400

    Added animation video

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -1,10 +1,36 @@
+import { useRef } from "react";
+
+
 const Animation = () => {
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleOverlayClcik = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }
+  };
   return (
-    <div className="bg-black h-[100vh]  mt-[710px] relative">
-      <div className="text-white text-center pt-[450px] text-3xl">
-      Animation goes here
+    <div className="bg-black sm:h-[98vh] mt-[700px] relative">
+     
+        <video
+        ref={videoRef}
+          src="https://cloud-ofkfgjclu-hack-club-bot.vercel.app/0ivoine_journey_sound.mp4"
+          width="100%"
+        className="h-[100vh]"
+        
+        >
+     
+        </video>
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60 cursor-pointer" 
+        onClick={handleOverlayClcik}
+        ></div>
       </div>
-    </div>
+
   );
 }
 
